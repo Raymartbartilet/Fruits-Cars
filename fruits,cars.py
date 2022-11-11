7b0bdd8c73634a74dbfd78ca4c278eae49360d8a
Raymart Bartilet <114345498+Raymartbartilet@users.noreply.github.com>
2022-11-11 16:02:15 +0800

    Add files via upload

---
+++ fruits,cars.py
@@ -0,0 +1,38 @@
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+fruits = (" apple", "banana", "cherry")
+print(len(fruits))
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+fruits = ("apple", "banana", "cherry")
+
+car =	{ "brand": "Ford",
+ "model": "Mustang",
+  "year": 1964}
+  
+print(car.get("model"))
+
+car =	{ "brand": "Ford",
+ "model": "Mustang", 
+ "year": 1964}
+car["year"] = 2020
+
+car =	{ "brand": "Ford",
+ "model": "Mustang", 
+ "year": 1964}
+ 
+car["color"]="blue"
+
+car =	{ "brand": "Ford", 
+"model": "Mustang", 
+"year": 1964}
+
+car.pop("model")
+
+car =	{ "brand": "Ford",
+ "model": "Mustang", 
+ "year": 1964}
+ 
+car.clear()
